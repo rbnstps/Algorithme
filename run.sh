79e5fe2da7c2288af6d943b412e835ca23ecf8cd
rbnstps <rbnstps@gmail.com>
2021-04-30 16:19:43 +0200

    pseudocode runsh

--- run.sh
+++ run.sh
@@ -1,2 +1,13 @@
 echo "hallo"
-echo >> git status
+
+if added img detected
+    
+
+    #conversion
+    python dataset_tool.py create_from_images fractal_realities/tfrecords fractal_realities/dataset
+    
+    
+    #training
+    python run_training.py --num-gpus=1 --data-dir=fractal_realities/ --config=config-f --dataset=dataset --total-kimg 1 --gamma=1
+
+    
